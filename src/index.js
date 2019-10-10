a1a3d0baa40c6f0850a281a5af3b45573f0b9064
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-11 02:00:24 +0300

    feat: solve 0/22

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function zeros(expression) {
   // your solution
-}
+  let expressionArray = [];
+  let count;
+  let r = /\d+/; //regexp for numbers
+  
+    if (expression.indexOf('*') !== -1) {
+      expressionArray = expression.split('*');
+      } else {
+        expressionArray = expression;
+      }
+      console.log(expressionArray);
+
+      expressionArray.forEach(element => {
+        if (element[element.length - 2] === '!') { // checking for type of factorial
+          element = element.match(r);
+          console.log('element: ' + element);
+          if (element%2 === 0) {
+            // console.log('remainder: ' + element%2); //остаток 
+            while (element > 0) {
+              console.log('element: ' + element);
+              element /= 10;
+              count += element;
+              // console.log('count: ' + count);
+            } 
+          }
+        } else {
+          while (element > 0) {
+            element /= 5;
+            count += element;
+          }
+        }
+          
+      });
+      
+      return count;
+    }

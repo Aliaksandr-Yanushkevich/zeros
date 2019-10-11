d55cafbffe48ce313ea6e9f523184b1bb5b60acb
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-11 21:40:00 +0300

    feat: solve 4/22

--- src/index.js
+++ src/index.js
@@ -14,26 +14,31 @@ module.exports = function zeros(expression) {
       expressionArray.forEach(element => {
         if (element[element.length - 2] === '!' && element.match(r)%2 === 0) { // checking for type of factorial and count 2
           for (i = 2; i <= element.match(r); i *= 2) {        
-            countOf2 += element.match(r)/i;
+            countOf2 += Math.floor(element.match(r)/i);
             console.log('countOf2: ' + countOf2);
           }
+        } else if (element[element.length - 2] !== '!') {  // single factorial
+          for (i = 2; i <= element.match(r); i *= 2) {
+            countOf2 += Math.floor(element.match(r)/i);
+            console.log('countOf2: ' + countOf2);
+          }
+        } else {
+          countOf2 += 0; 
         }
 
-        if (element[element.length - 2] !== '!') {  // single factorial
-          for (i = 2; i <= element.match(r); i *= 2) {
-            countOf2 += element.match(r)/i;
-            console.log('countOf2: ' + countOf2);
-          }
-        }
+        
               
         if (element[element.length - 2] === '!' && element.match(r)%2 === 0) {
           for (i = 5; i <= element.match(r); i *= 5) {
-            countOf5 += element.match(r)/i/2;
+            countOf5 += Math.floor(Math.floor(element.match(r)/i)/2);
+            console.log('countOf5: ' + countOf5);
         }
       } else if (element[element.length - 2] === '!' && element.match(r)%2 !== 0) {
-        countOf5 += element.match(r)/i/2;
+        countOf5 += Math.ceil(Math.floor(element.match(r)/i)/2);
+        console.log('countOf5: ' + countOf5);
       } else {
-        countOf5 += element.match(r)/i;
+        countOf5 +=Math.floor(element.match(r)/i);
+        console.log('countOf5: ' + countOf5);
       }
       });
       return Math.min(countOf2, countOf5)
